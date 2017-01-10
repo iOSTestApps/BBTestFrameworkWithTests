2a23af7c0c81a1e0f071003748b955e7dcd98cfa
Christopher Stott <christopherstott@gmail.com>
2017-01-10 15:59:11 -0800

    Initial Commit

---
+++ BBTestFrameworkWithTests/BBTestFrameworkWithTests.h
@@ -0,0 +1,19 @@
+//
+//  BBTestFrameworkWithTests.h
+//  BBTestFrameworkWithTests
+//
+//  Created by Chris Stott on 2017-01-10.
+//  Copyright © 2017 Chris Stott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBTestFrameworkWithTests.
+FOUNDATION_EXPORT double BBTestFrameworkWithTestsVersionNumber;
+
+//! Project version string for BBTestFrameworkWithTests.
+FOUNDATION_EXPORT const unsigned char BBTestFrameworkWithTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBTestFrameworkWithTests/PublicHeader.h>
+
+
